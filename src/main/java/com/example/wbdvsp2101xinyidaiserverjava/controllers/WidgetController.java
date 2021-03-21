3aeed3c8e7737d478b604076fadbf75eb82160db
XinyiDai1017 <daixinyi96@gmail.com>
2021-03-20 23:00:48 -0700

    add widget controllers, models, services

---
+++ src/main/java/com/example/wbdvsp2101xinyidaiserverjava/controllers/WidgetController.java
@@ -0,0 +1,53 @@
+package com.example.wbdvsp2101xinyidaiserverjava.controllers;
+
+import com.example.wbdvsp2101xinyidaiserverjava.models.Widget;
+import com.example.wbdvsp2101xinyidaiserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+    @Autowired
+    WidgetService service;// = new WidgetService();
+
+    @PostMapping("/api/topics/{tid}/widgets")
+    public Widget createWidget(
+            @PathVariable("tid") String tid,
+            @RequestBody Widget widget) {
+        //widget.setTopicId(topicId);
+        return service.createWidget(tid, widget);
+    }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(
+            @PathVariable("tid") String tid
+    ) {
+        return service.findWidgetsForTopic(tid);
+    }
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/widgets/{wid}")
+    public Widget findWidgetById(
+            @PathVariable("wid") Long wid) {
+        return service.findWidgetById(wid);
+    }
+
+    @DeleteMapping("/api/widgets/{wid}")
+    public Integer deleteWidget(@PathVariable("wid") Long wid) {
+        return service.deleteWidget(wid);
+    }
+
+    @PutMapping("/api/widgets/{wid}")
+    public Integer updateWidget(
+            @PathVariable("wid") Long wid,
+            @RequestBody Widget widget) {
+        return service.updateWidget(wid, widget);
+    }
+}
---
+++ src/main/java/com/example/wbdvsp2101xinyidaiserverjava/models/Widget.java
@@ -0,0 +1,162 @@
+package com.example.wbdvsp2101xinyidaiserverjava.models;
+
+public class Widget {
+    private String topicId;
+    private String name;
+    private Long id;
+    private String type;
+    private Integer widgetOrder;
+    private String text;
+    private String src;
+    private String url;
+    private String href;
+    private Integer size;
+    private Integer width;
+    private Integer height;
+    private String cssClass;
+    private String style;
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWidgetOrder() {
+        return widgetOrder;
+    }
+
+    public void setWidgetOrder(Integer widgetOrder) {
+        this.widgetOrder = widgetOrder;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Widget() {
+    }
+
+    public Widget(Long id, String name, String topicId, String type, Integer widgetOrder, Integer size, String text, String src, String url, String href, Integer width, Integer height,
+String cssClass, String style, String value) {
+        this.id = id;
+        this.name = name;
+        this.topicId = topicId;
+        this.type = type;
+        this.widgetOrder = widgetOrder;
+        this.size = size;
+        this.text = text;
+        this.src = src;
+        this.url = url;
+        this.href = href;
+        this.width = width;
+        this.height = height;
+        this.cssClass = cssClass;
+        this.style = style;
+        this.value = value;
+    }
+}
